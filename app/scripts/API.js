e2fddb87ea7eff83c30deea275092158ab4aae67
Raphael Cruzeiro <raphaelcruzeiro@raphaelcruzeiro.com>
2014-04-25 19:24:55 -0300

    Correct API endpoint.

--- app/scripts/API.js
+++ app/scripts/API.js
@@ -3,7 +3,8 @@
 (function() {
 	var module = angular.module('API', []);
 	module.service('API', function() {
-		this.baseUrl = 'http://127.0.0.1:8111';
+		//this.baseUrl = 'http://127.0.0.1:8111';
+		this.baseUrl = 'http://raphaelcruzeiro.eu';
 		this.contact = [this.baseUrl, 'contact/'].join('/');
 	});
 })();
